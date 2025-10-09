2bb18814903a61210ff6f71c0fecdeab6db37aa5
A1aaSayed <alaa02.sayed02@gmail.com>
2025-10-09 12:18:26 +0300

    update controllers

---
+++ src/controllers/DataController.py
@@ -0,0 +1,44 @@
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+from fastapi import UploadFile
+# from models import ResponseSignal
+from core import ResponseSignal
+import re
+import os
+
+class DataController(BaseController):
+    def __init__(self):
+        super().__init__()
+        self.size_scale = 1048576 # Convert MB to bytes
+    
+    def validate_uploaded_file(self, file: UploadFile):
+        if file.content_type not in self.app_settings.FILE_ALLOWD_TYPES:
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
+
+        return True, ResponseSignal.FILE_UPLOADED_SUCCESS.value
+    
+    def generate_unique_filename(self, origin_filename:str, project_id: str):
+        random_filename = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+        cleaned_filename = self.get_clean_filename(origin_filename=origin_filename)
+
+        new_file_path = os.path.join(
+            project_path, random_filename + '_' + cleaned_filename
+        )
+
+        while os.path.exists(new_file_path):
+            random_filename = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_filename + '_' + cleaned_filename
+            )
+
+        return new_file_path
+
+    def get_clean_filename(self, origin_filename: str):
+        cleaned_filename = re.sub(r'[^\w.]', '', origin_filename.strip())
+        cleaned_filename = cleaned_filename.replace(' ', '_')
+        return cleaned_filename
